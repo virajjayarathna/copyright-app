425fe5405b21481b06cbf1188ddc0045528fb4fa
virajjayarathna <virajmjayarathna@gmail.com>
2025-04-07 22:08:17 +0530

    update src 11

--- src/index.js
+++ src/index.js
@@ -171,8 +171,6 @@ console.log(modifiedContent);
 async function saveCopyrightInfo(octokit, repoOwner, repoName, branch, encryptionDetails, commitSha) {
   try {
     const infoContent = 
-      `Project Name: ${encryptionDetails.projectName}\n` +
-      `Key: ${encryptionDetails.encryptionKey}\n` +
       `Full Encrypted: ${encryptionDetails.encryptedString}\n`;
     const { data: blobData } = await octokit.git.createBlob({
       owner: repoOwner,
